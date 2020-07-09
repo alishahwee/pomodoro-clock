877b6d336b14fe933555074b7084acb6d3228d00
alishahwee <alisha.hwee@outlook.com>
2020-07-09 16:21:14 -0500

    Stylize header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import headerImg from '../images/header.png';
+
+function Header() {
+  return (
+    <div className='Header'>
+      <img src={headerImg} alt='header' />
+    </div>
+  );
+}
+
+export default Header;
