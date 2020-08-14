7404db9ef46255f62b1de383968294e2f281a7a7
alishahwee <alisha.hwee@outlook.com>
2020-08-14 00:29:41 -0500

    Pass clockify to props

--- src/components/SessionLength.js
+++ src/components/SessionLength.js
@@ -4,7 +4,7 @@ function SessionLength(props) {
   return (
     <div class='SessionLength'>
       <span id='timer-label'>{props.timerMode}</span>
-      <span id='time-left'></span>
+      <span id='time-left'>{props.clockify()}</span>
     </div>
   );
 }
