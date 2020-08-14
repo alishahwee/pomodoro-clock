1fcf66e64e8fbdbcd13565d2657360863691b407
alishahwee <alisha.hwee@outlook.com>
2020-08-14 00:29:04 -0500

    Add clockify function

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -9,12 +9,23 @@ class Timer extends Component {
       sessionLength: 25,
       timerOn: false,
       timerMode: 'Session',
+      timer: 1500,
     }
+    this.clockify = this.clockify.bind(this);
   }
+
+  clockify() {
+    let minutes = Math.floor(this.state.timer / 60);
+    let seconds = this.state.timer - minutes * 60;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    return minutes + ':' + seconds;
+  }
+  
   render() {
     return (
       <div className='Timer'>
-        <SessionLength timerMode={this.state.timerMode} />
+        <SessionLength timerMode={this.state.timerMode} clockify={this.clockify} />
       </div>
     );
   }
