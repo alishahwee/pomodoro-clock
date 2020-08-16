63625e4d0a6dfd0bb4b165781222917b5f004033
alishahwee <Alisha.Hwee@outlook.com>
2020-08-15 22:26:18 -0500

    Code time-length-control boilerplate

--- src/components/TimerLengthControl.js
+++ src/components/TimerLengthControl.js
@@ -1,8 +1,27 @@
 import React from 'react';
 
-function TimerLengthControl() {
+function TimerLengthControl(props) {
   return (
-    <div className='TimerLengthControl'></div>
+    <div className='TimerLengthControl'>
+      <p id={props.titleId}>{props.title}</p>
+      <button
+        id={props.minId}
+        className="btn-level"
+        value="-"
+        onClick={props.onClick}
+      >
+        <i className="fas fa-minus" />
+      </button>
+      <p id={props.lengthId}>{props.length}</p>
+      <button
+        id={props.addId}
+        className="btn-level"
+        value="+"
+        onClick={props.onClick}
+      >
+        <i className="fas fa-plus" />
+      </button>
+    </div>
   );
 }
 
