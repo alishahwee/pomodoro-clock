cf3d33ef2c6ecbf3b466edfbd19fdf3ed838a791
alishahwee <Alisha.Hwee@outlook.com>
2020-08-16 04:00:33 -0500

    Add controls div

--- src/components/SessionLength.js
+++ src/components/SessionLength.js
@@ -5,6 +5,10 @@ function SessionLength(props) {
     <div class='SessionLength'>
       <span id='timer-label'>{props.timerMode}</span>
       <span id='time-left'>{props.clockify()}</span>
+      <div className='SessionLength__controls'>
+        <button id='start_stop'>{props.btnState}</button>
+        <i id='reset' className='fas fa-redo' />
+      </div>
     </div>
   );
 }
