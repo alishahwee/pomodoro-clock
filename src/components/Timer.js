85c0c6791e705c01fe119a39001d74284026fa89
alishahwee <Alisha.Hwee@outlook.com>
2020-08-16 04:01:33 -0500

    Mount TimerLength components, set state for btn

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SessionLength from './SessionLength';
+import TimerLengthControl from './TimerLengthControl';
 
 class Timer extends Component {
   constructor(props) {
@@ -7,11 +8,25 @@ class Timer extends Component {
     this.state = {
       breakLength: 5,
       sessionLength: 25,
+      btnState: 'Start',
       timerOn: false,
       timerMode: 'Session',
       timer: 1500,
-    }
+    };
     this.clockify = this.clockify.bind(this);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState === this.state) return;
+    if (prevState.timerOn) {
+      this.setState({
+        btnState: 'Stop',
+      });
+    } else {
+      this.setState({
+        btnState: 'Start',
+      });
+    }
   }
 
   clockify() {
@@ -21,11 +36,31 @@ class Timer extends Component {
     minutes = minutes < 10 ? '0' + minutes : minutes;
     return minutes + ':' + seconds;
   }
-  
+
   render() {
     return (
       <div className='Timer'>
-        <SessionLength timerMode={this.state.timerMode} clockify={this.clockify} />
+        <SessionLength
+          timerMode={this.state.timerMode}
+          clockify={this.clockify}
+          btnState={this.state.btnState}
+        />
+        <TimerLengthControl
+          titleId='break-label'
+          minId='break-decrement'
+          addId='break-increment'
+          lengthId='break-length'
+          title='Break Length'
+          length={this.state.breakLength}
+        />
+        <TimerLengthControl
+          titleId='session-label'
+          minId='session-decrement'
+          addId='session-increment'
+          lengthId='session-length'
+          title='Session Length'
+          length={this.state.sessionLength}
+        />
       </div>
     );
   }
