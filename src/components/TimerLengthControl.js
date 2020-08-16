3d83bab70b655fc597bb9810365679d18d8c7b7a
alishahwee <Alisha.Hwee@outlook.com>
2020-08-16 03:17:41 -0500

    Prettify and implement BEM

--- src/components/TimerLengthControl.js
+++ src/components/TimerLengthControl.js
@@ -3,23 +3,25 @@ import React from 'react';
 function TimerLengthControl(props) {
   return (
     <div className='TimerLengthControl'>
-      <p id={props.titleId}>{props.title}</p>
+      <p id={props.titleId} className='TimerLengthControl__title'>
+        {props.title}
+      </p>
       <button
         id={props.minId}
-        className="btn-level"
-        value="-"
+        className='TimerLengthControl__btn'
+        value='-'
         onClick={props.onClick}
       >
-        <i className="fas fa-minus" />
+        <i className='fas fa-minus' />
       </button>
       <p id={props.lengthId}>{props.length}</p>
       <button
         id={props.addId}
-        className="btn-level"
-        value="+"
+        className='TimerLengthControl__btn'
+        value='+'
         onClick={props.onClick}
       >
-        <i className="fas fa-plus" />
+        <i className='fas fa-plus' />
       </button>
     </div>
   );
