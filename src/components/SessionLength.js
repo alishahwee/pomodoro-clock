020050b9dbd78cc032ca0b8ba4b01e7a492ab50e
alishahwee <Alisha.Hwee@outlook.com>
2020-08-17 02:25:45 -0500

    Prettify, add missing tags, implement BEM

--- src/components/SessionLength.js
+++ src/components/SessionLength.js
@@ -2,12 +2,26 @@ import React from 'react';
 
 function SessionLength(props) {
   return (
-    <div class='SessionLength'>
-      <span id='timer-label'>{props.timerMode}</span>
-      <span id='time-left'>{props.clockify()}</span>
+    <div className='SessionLength'>
+      <p id='timer-label' className='SessionLength__label'>
+        {props.timerMode}
+      </p>
+      <span id='time-left' className='SessionLength__time'>
+        {props.clockify()}
+      </span>
       <div className='SessionLength__controls'>
-        <button id='start_stop'>{props.btnState}</button>
-        <i id='reset' className='fas fa-redo' />
+        <button
+          id='start_stop'
+          className='SessionLength__controls__btn SessionLength__controls__btn--start-stop'
+        >
+          {props.btnState}
+        </button>
+        <button
+          id='reset'
+          className='SessionLength__controls__btn SessionLength__controls__btn--reset'
+        >
+          <i className='fas fa-redo' />
+        </button>
       </div>
     </div>
   );
