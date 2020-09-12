cae9f03684b6059133acb0c2e136d84ba2a9d71f
alishahwee <alisha.hwee@outlook.com>
2020-09-12 00:21:55 -0500

    Chanage state according to component updates

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -17,7 +17,7 @@ class Timer extends Component {
     };
     this.controlTimer = this.controlTimer.bind(this);
     this.beginCountdown = this.beginCountdown.bind(this);
-    this.switchTimer = this.switchTimer.bind(this);
+    this.switchMode = this.switchMode.bind(this);
     this.decreaseTimer = this.decreaseTimer.bind(this);
     this.setBreakLength = this.setBreakLength.bind(this);
     this.setSessionLength = this.setSessionLength.bind(this);
@@ -26,17 +26,49 @@ class Timer extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    const timerOnDidUpdate = prevState.timerOn !== this.state.timerOn;
-
-    if (this.state.timerOn && timerOnDidUpdate) {
-      this.setState({
-        btnState: 'Stop',
-      });
-    } else if (timerOnDidUpdate) {
-      this.setState({
-        btnState: 'Start',
-      });
+    // Change button label based on timer being on or off
+    if (prevState.timerOn !== this.state.timerOn) {
+      if (this.state.timerOn) {
+        this.setState({
+          btnState: 'Stop',
+        });
+      } else {
+        this.setState({
+          btnState: 'Start',
+        });
+      }
     }
+
+    // Buzz when timer hits zero
+    if (this.state.timer === 0) {
+      this.alarm.play();
+    }
+
+    // Alternate timer mode whenever break/session hits zero
+    if (this.state.timer < 0) {
+      if (this.state.timerMode === 'Session') {
+        this.state.intervalID && this.state.intervalID.clear();
+        this.beginCountdown();
+        this.switchMode(this.state.breakLength, 'Break');
+      } else if (this.state.timerMode === 'Break') {
+        this.state.intervalID && this.state.intervalID.clear();
+        this.beginCountdown();
+        this.switchMode(this.state.sessionLength, 'Session');
+      }
+    }
+
+    // Change timer length based on mode when timer is off
+    if (!this.state.timerOn) {
+      if (prevState.breakLength !== this.state.breakLength) {
+        if (this.state.timerMode === 'Break') {
+          this.setState({ timer: this.state.breakLength * 60 });
+        }
+      } else if (prevState.sessionLength !== this.state.sessionLength) {
+        if (this.state.timerMode === 'Session') {
+          this.setState({ timer: this.state.sessionLength * 60 });
+        }
+      }
+    } 
   }
 
   controlTimer() {
@@ -49,18 +81,23 @@ class Timer extends Component {
     }
   }
 
+  // Use Accurate-Timer to call decreaseTimer every one second and assign it to state
   beginCountdown() {
     this.setState({
       intervalID: accurateInterval(this.decreaseTimer, 1000),
     });
   }
 
-  switchTimer() {
-    // TODO
+  switchMode(time, mode) {
+    this.setState({
+      timer: time * 60,
+      timerMode: mode,
+    });
   }
 
+  // Decrease timer (sec) by one
   decreaseTimer() {
-    this.setState((prevState) => ({timer: prevState.timer - 1}));
+    this.setState((prevState) => ({ timer: prevState.timer - 1 }));
   }
 
   resetTimer() {
@@ -74,6 +111,8 @@ class Timer extends Component {
       intervalID: undefined,
     });
     this.state.intervalID && this.state.intervalID.clear();
+    this.alarm.pause();
+    this.alarm.currentTime = 0;
   }
 
   setBreakLength(e) {
@@ -90,7 +129,7 @@ class Timer extends Component {
 
   setSessionLength(e) {
     if (this.state.timerOn) return; // Disable if timer is on
-    const currentLength = this.state.breakLength;
+    const currentLength = this.state.sessionLength;
     const value = e.currentTarget.value;
 
     if (value === '-' && currentLength !== 1) {
@@ -100,6 +139,7 @@ class Timer extends Component {
     }
   }
 
+  // Turn seconds into analog time
   clockify() {
     let minutes = Math.floor(this.state.timer / 60);
     let seconds = this.state.timer - minutes * 60;
@@ -141,7 +181,7 @@ class Timer extends Component {
           preload='auto'
           src='https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav'
           ref={(audio) => {
-            this.audioBeep = audio;
+            this.alarm = audio;
           }}
         />
       </div>
