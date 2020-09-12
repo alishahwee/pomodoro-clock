0d413f68e60dbbe7fa2b732aab083568b9ec5196
alishahwee <alisha.hwee@outlook.com>
2020-09-12 00:54:47 -0500

    Updates

---
+++ precache-manifest.af6fe6f71b85126cfd989927baf2724a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd5f409f11534e7790c4fa36d8351baa",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "39d45aada9b3d972536b",
+    "url": "/pomodoro-clock/static/css/2.8ff17cee.chunk.css"
+  },
+  {
+    "revision": "2ce92f48f4fe9da607a4",
+    "url": "/pomodoro-clock/static/css/main.a23d128a.chunk.css"
+  },
+  {
+    "revision": "39d45aada9b3d972536b",
+    "url": "/pomodoro-clock/static/js/2.d627e080.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.d627e080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ce92f48f4fe9da607a4",
+    "url": "/pomodoro-clock/static/js/main.ba82a8e6.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  },
+  {
+    "revision": "f87043b83d3540224f1b2b2f3e58bb37",
+    "url": "/pomodoro-clock/static/media/header.f87043b8.png"
+  }
+]);
